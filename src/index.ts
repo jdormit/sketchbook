8d87bb8c278cc95ff9628dd400a0006e24407cee
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-09-12 17:59:55 -0400

    Capitalize interface

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-interface sketch {
+interface Sketch {
     title: string,
     module: string
 }
 
-const sketches : sketch[] = [
+const sketches : Sketch[] = [
     { title: "Splatter", module: "splatter1" }
 ];
 
 define(require => {
     const p5 = require("./lib/p5");
 
-    const hydrateTitleSelector = function($titleSelector: HTMLSelectElement, sketches: sketch[]) : HTMLSelectElement {
+    const hydrateTitleSelector = function($titleSelector: HTMLSelectElement, sketches: Sketch[]) : HTMLSelectElement {
         $titleSelector.innerHTML = "";
         sketches.forEach(sketch => {
             const $option = new Option(sketch.title, sketch.module);

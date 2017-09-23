e81de64ec13df29b79daa63d9066416d4578de73
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-09-23 06:40:56 -0400

    Implement sketch list

--- public/js/index.js
+++ public/js/index.js
@@ -17,48 +17,59 @@ var initSeedSpan = function (seed) {
     var $seed = document.getElementById("seed");
     $seed.innerHTML = "Seed: " + seed;
 };
-var initSketchList = function () {
-    var $sketchList = document.getElementById("sketchList");
-    for (var _i = 0, sketches_1 = sketches; _i < sketches_1.length; _i++) {
-        var sketch = sketches_1[_i];
-        var $text = document.createTextNode(sketch.title);
-        var $li = document.createElement("li");
-        $li.appendChild($text);
-        $sketchList.appendChild($li);
-    }
+var initTitle = function (title) {
+    var $title = document.getElementById("title");
+    $title.innerHTML = "";
+    $title.appendChild(document.createTextNode(title));
 };
-var loadSketch = function (explicitSeed) {
-    var $titleSelector = document.getElementById("titleSelector");
-    var sketchData = sketches[$titleSelector.selectedIndex];
+var displaySketchList = function () {
+    var $listHolder = document.getElementById("listHolder");
+    $listHolder.style.display = "block";
+};
+var hideSketchList = function () {
+    var $listHolder = document.getElementById("listHolder");
+    $listHolder.style.display = "none";
+};
+var currentSketch = sketches[0];
+var loadSketch = function (sketchData, explicitSeed) {
+    currentSketch = sketchData;
     var seed = explicitSeed
         ? explicitSeed
         : typeof sketchData.seed === "function"
             ? sketchData.seed()
             : sketchData.seed;
     initSeedSpan(seed);
+    initTitle(sketchData.title);
     require(["./lib/p5", "./sketches/" + sketchData.module], function (p5, sketch) {
         var currentP5 = new p5(sketch.default(seed));
         initSaveButton(currentP5, sketchData.title);
     });
 };
+var initSketchList = function () {
+    var $sketchList = document.getElementById("sketchList");
+    var _loop_1 = function (sketch) {
+        var $text = document.createTextNode(sketch.title);
+        var $li = document.createElement("li");
+        $li.addEventListener("click", function () {
+            loadSketch(sketch);
+            hideSketchList();
+        });
+        $li.appendChild($text);
+        $sketchList.appendChild($li);
+    };
+    for (var _i = 0, sketches_1 = sketches; _i < sketches_1.length; _i++) {
+        var sketch = sketches_1[_i];
+        _loop_1(sketch);
+    }
+};
 define(function (require) {
     var p5 = require("./lib/p5");
-    var sketchTitle;
-    var sketchModule;
-    var initTitleSelector = function ($titleSelector, sketches) {
-        $titleSelector.innerHTML = "";
-        sketches.forEach(function (sketch) {
-            var $option = new Option(sketch.title, sketch.module);
-            $titleSelector.add($option);
-        });
-        $titleSelector.item(0).selected = true;
-        $titleSelector.addEventListener("change", function () { return loadSketch(); });
-        return $titleSelector;
-    };
-    var $titleSelector = document.getElementById("titleSelector");
-    initTitleSelector($titleSelector, sketches);
     initSketchList();
-    loadSketch();
+    loadSketch(sketches[0]);
+    var $list = document.getElementById("list");
+    $list.addEventListener("click", displaySketchList);
+    var $closeList = document.getElementById("closeList");
+    $closeList.addEventListener("click", hideSketchList);
     var $refresh = document.getElementById("refresh");
-    $refresh.addEventListener("click", function () { return loadSketch(); });
+    $refresh.addEventListener("click", function () { return loadSketch(currentSketch); });
 });
--- src/index.ts
+++ src/index.ts
@@ -32,60 +32,67 @@ const initSeedSpan = function(seed: string | number) {
     $seed.innerHTML = `Seed: ${seed}`;
 };
 
-const initSketchList = function() {
-    const $sketchList = document.getElementById("sketchList")!;
-    for (const sketch of sketches) {
-        const $text = document.createTextNode(sketch.title);
-        const $li = document.createElement("li");
-        $li.appendChild($text);
-        $sketchList.appendChild($li);
-    }
+const initTitle = function(title: string) {
+    const $title = document.getElementById("title")!;
+    $title.innerHTML = "";
+    $title.appendChild(document.createTextNode(title));
 };
 
-const loadSketch = function(explicitSeed?: number | string) {
-    const $titleSelector: HTMLSelectElement = <HTMLSelectElement>document.getElementById(
-        "titleSelector"
-    );
-    const sketchData = sketches[$titleSelector.selectedIndex];
+const displaySketchList = function() {
+    const $listHolder = document.getElementById("listHolder")!;
+    $listHolder.style.display = "block";
+};
+
+const hideSketchList = function() {
+    const $listHolder = document.getElementById("listHolder")!;
+    $listHolder.style.display = "none";
+};
+
+let currentSketch: Sketch = sketches[0];
+
+const loadSketch = function(
+    sketchData: Sketch,
+    explicitSeed?: number | string
+) {
+    currentSketch = sketchData;
     const seed = explicitSeed
         ? explicitSeed
         : typeof sketchData.seed === "function"
           ? sketchData.seed()
           : sketchData.seed;
     initSeedSpan(seed);
+    initTitle(sketchData.title);
     require(["./lib/p5", "./sketches/" + sketchData.module], (p5, sketch) => {
         const currentP5 = new p5(sketch.default(seed));
         initSaveButton(currentP5, sketchData.title);
     });
 };
 
+const initSketchList = function() {
+    const $sketchList = document.getElementById("sketchList")!;
+    for (const sketch of sketches) {
+        const $text = document.createTextNode(sketch.title);
+        const $li = document.createElement("li");
+        $li.addEventListener("click", () => {
+            loadSketch(sketch);
+            hideSketchList();
+        });
+        $li.appendChild($text);
+        $sketchList.appendChild($li);
+    }
+};
+
 define(require => {
     const p5 = require("./lib/p5");
 
-    let sketchTitle;
-    let sketchModule;
-
-    const initTitleSelector = function(
-        $titleSelector: HTMLSelectElement,
-        sketches: Sketch[]
-    ): HTMLSelectElement {
-        $titleSelector.innerHTML = "";
-        sketches.forEach(sketch => {
-            const $option = new Option(sketch.title, sketch.module);
-            $titleSelector.add($option);
-        });
-        $titleSelector.item(0).selected = true;
-        $titleSelector.addEventListener("change", () => loadSketch());
-        return $titleSelector;
-    };
-
-    const $titleSelector: HTMLSelectElement = <HTMLSelectElement>document.getElementById(
-        "titleSelector"
-    );
-    initTitleSelector($titleSelector, sketches);
     initSketchList();
-    loadSketch();
+    loadSketch(sketches[0]);
+
+    const $list = document.getElementById("list")!;
+    $list.addEventListener("click", displaySketchList);
+    const $closeList = document.getElementById("closeList")!;
+    $closeList.addEventListener("click", hideSketchList);
 
     const $refresh = document.getElementById("refresh")!;
-    $refresh.addEventListener("click", () => loadSketch());
+    $refresh.addEventListener("click", () => loadSketch(currentSketch));
 });
